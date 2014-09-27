6212b19537d233e3f22d8668c18c0b8d41a13494
Shawn McCormick <wsmkd6@mst.edu>
2014-09-27 10:56:01 -0500

    Add basic movement

--- AI.py
+++ AI.py
@@ -7,18 +7,50 @@ class AI(BaseAI):
   """The class implementing gameplay logic."""
   @staticmethod
   def username():
-    return "Shell AI"
+    return "S"
 
   @staticmethod
   def password():
     return "password"
 
   CLAW, ARCHER, REPAIRER, HACKER, TURRET, WALL, TERMINATOR, HANGAR = range(8)
-
-  ##This function is called once, before your first turn
+  
   def init(self):
-    self.players[self.playerID].orbitalDrop(0, 0, randint(0,7))
-    return
+    pass
+
+  def mySide(self):
+    if self.playerID == 0:
+      return 0
+    return self.mapWidth - 1
+
+  def tile(self, x, y):
+    return self.tiles[y*self.mapHeight + x]
+
+  def spawn(self):
+    y = random.randint(0,self.mapHeight - 1)
+    self.players[self.playerID].orbitalDrop(self.mySide(), y, random.randint(0,1))
+
+    return 1
+  
+  def move_units(self):
+    for droid in self.droids:
+      if droid.movementLeft > 0 and droid.owner == self.playerID:
+        if self.turnNumber % 3 == 0 and self.tile(droid.forward, droid.y).variantToAssemble == -1:
+          droid.move(droid.forward, droid.y)
+        elif self.turnNumber % 3 == 1:
+          droid.move(droid.x, droid.y + random.randint(-1,1))
+        if self.turnNumber % 3 == 2 and self.tile(droid.forward, droid.y).variantToAssemble == -1:
+          droid.move(droid.forward, droid.y)
+    print("WORDS WORDS WORDS WORDS TILE ({}).".format(self.tile(0,0)))
+    
+    return 1
+
+  def attack(self):
+    for droid in self.droids:
+      if droid.movementLeft > 0 and droid.owner == self.playerID and self.tile(droid.forward, droid.y).variantToAssemble == -1:
+        droid.operate(droid.forward, droid.y)
+        print("Droid {} operating on {}".format(droid.id, self.tile(droid.forward, droid.y)))
+    pass
 
   ##This function is called once, after your last turn
   def end(self):
@@ -27,7 +59,9 @@ class AI(BaseAI):
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
-  	self.spawn()
+    self.spawn()
+    self.move_units()
+    self.attack()
     return 1
 
   def __init__(self, conn):
